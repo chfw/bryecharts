019fe28a24befa859e96a093cbc1455fe9560dbc
chfw <wangc_2011@hotmail.com>
2017-10-16 17:40:39 +0100

    output data into dist folder

--- build.sh
+++ build.sh
@@ -4,4 +4,6 @@ python -m brython --install
 cd ..
 bp local/brython_stdlib.js bp-requirements.txt editor.py pyecharts
 rm -rf local
+cp brython_modules.js public/js
+mv brython_modules.* dist/
 echo "Done"
