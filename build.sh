dd512026fd4b54b1d5c52f4522989040a62fd4e7
chfw <wangc_2011@hotmail.com>
2017-10-16 21:33:00 +0100

    first ever pyecharts.js release with pyecharts v0.2.6 baseline, https://github.com/chenjiandongx/pyecharts/issues/210

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ python -m brython --install
 cd ..
 bp local/brython_stdlib.js bp-requirements.txt editor.py pyecharts
 rm -rf local
-cp brython_modules.js public/js
+cp brython_modules.js public/js/pyecharts.js
 mv brython_modules.js npm/pyecharts.js
 mv brython_modules.log.txt changelog/
 echo "Done"
