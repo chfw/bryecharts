3825b3d4c98debd44d7d4fb8a5f44334c91fbcc1
chfw <wangc_2011@hotmail.com>
2017-10-16 18:06:34 +0100

    :art: rename it too

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ cd ..
 bp local/brython_stdlib.js bp-requirements.txt editor.py pyecharts
 rm -rf local
 cp brython_modules.js public/js
-mv brython_modules.js npm/bryecharts.js
+mv brython_modules.js npm/pyecharts.js
 mv brython_modules.log.txt changelog/
 echo "Done"
