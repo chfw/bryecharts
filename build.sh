7c74b413239f5041bed0c937132fe1ca5bfe497c
chfw <wangc_2011@hotmail.com>
2017-10-18 23:18:26 +0100

    :horse: another speed boost by concatenate minify javascripts and css files into one file so as to get files in fewer http requests

--- build.sh
+++ build.sh
@@ -7,4 +7,7 @@ rm -rf local
 cp brython_modules.js public/js/pyecharts.js
 mv brython_modules.js npm/pyecharts.js
 mv brython_modules.log.txt changelog/
+npm install
+gulp
+cp README.md npm/
 echo "Done"
